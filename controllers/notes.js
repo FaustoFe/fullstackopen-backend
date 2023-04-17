ba1c0f24bd5b587987ca1a37f2cbaefd2f1cdebd
FaustoFe <fausto.2000.22@gmail.com>
2023-04-17 11:10:50 -0300

    change promises to async/await

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,41 +1,41 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (req, res) => {
-  Note.find({}).then(notes => {
-    res.json(notes)
-  })
+notesRouter.get('/', async (req, res) => {
+  const notes = await Note.find({})
+  res.json(notes)
 })
 
-notesRouter.get('/:id', (req, res, next) => {
-  Note.findById(req.params.id)
-    .then(note => {
-      if (note) {
-        res.json(note)
-      } else {
-        res.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+notesRouter.get('/:id', async (req, res, next) => {
+  try {
+    const note = await Note.findById(req.params.id)
+    if (note) {
+      res.json(note)
+    } else {
+      res.status(404).end()
+    }
+  } catch(exception) {
+    next(exception)
+  }
 })
 
-notesRouter.post('/', (req, res, next) => {
+notesRouter.post('/', async (req, res, next) => {
   const body = req.body
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date(),
   })
 
-  note.save()
-    .then(savedNote => {
-      res.json(savedNote)
-    })
-    .catch(error => next(error))
+  try {
+    const savedNote = await note.save()
+    res.json(savedNote)
+  } catch(exception) {
+    next(exception)
+  }
 })
 
-notesRouter.put('/:id', (req, res, next) => {
+notesRouter.put('/:id', async (req, res, next) => {
   const { content, important } = req.body
 
   const note = {
@@ -43,19 +43,21 @@ notesRouter.put('/:id', (req, res, next) => {
     important,
   }
 
-  Note.findByIdAndUpdate(req.params.id, note, { new: true })
-    .then(updatedNote => {
-      res.json(updatedNote)
-    })
-    .catch(error => next(error))
+  try {
+    const updatedNote = await Note.findByIdAndUpdate(req.params.id, note, { new: true })
+    res.json(updatedNote)
+  } catch(exception) {
+    next(exception)
+  }
 })
 
-notesRouter.delete('/:id', (req, res, next) => {
-  Note.findByIdAndRemove(req.params.id)
-    .then(() => {
-      res.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (req, res, next) => {
+  try {
+    await Note.findByIdAndRemove(req.params.id)
+    res.status(204).end()
+  } catch(exception) {
+    next(exception)
+  }
 })
 
 module.exports = notesRouter
