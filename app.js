9fb1cbcdfc43e1f476bac4d7f3d558f4338af5d9
FaustoFe <fausto.2000.22@gmail.com>
2023-04-21 14:40:16 -0300

    add user and login routers

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const notesRouter = require('./controllers/notes')
-const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+require('express-async-errors')
+
+const userRouter = require('./controllers/users')
+const notesRouter = require('./controllers/notes')
+const loginRouter = require('./controllers/login')
+
+const middleware = require('./utils/middleware')
 
 logger.info('connecting to', config.DB_URI)
 
@@ -22,6 +27,8 @@ app.use(express.static('build'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 app.use('/api/notes', notesRouter)
 
 app.use(middleware.unknownEndpoint)
