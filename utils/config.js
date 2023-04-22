7d6b2d0cfb729bc30bd01575e0cce46a104790e6
FaustoFe <fausto.2000.22@gmail.com>
2023-04-22 10:53:45 -0300

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-let DB_URI= process.env.DB_URI
-if (process.env.NODE_ENV === 'test') {
+const NODE_ENV = process.env.NODE_ENV
+let DB_URI = process.env.DB_URI
+if (NODE_ENV === 'test') {
   DB_URI = process.env.TEST_DB_URI
 }
 
 module.exports = {
-  PORT, DB_URI
+  PORT, DB_URI, NODE_ENV
 }
